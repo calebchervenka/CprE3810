71415dc7b1c2d36f6e595c69b5fa0f7862211256
Caleb Chervenka <calebchervenka@gmail.com>
2026-03-25 11:53:06 -0500

    Reverted to boilerplate

--- riscv/addiseq.s
+++ riscv/addiseq.s
@@ -7,56 +7,14 @@
 
 # code/instruction section
 .text
-
-# addi
 addi  x1,  x0,  1 		# Place 1  in x1
 addi  x2,  x0,  2		# Place 2  in x2
 addi  x3,  x0,  3		# Place 3  in x3
 addi  x4,  x0,  4		# Place 4  in x4
 addi  x5,  x0,  5		# Place 5  in x5
-
-# addi (negative)
-addi  x6,  x0,  -1		# Place -1 in x6
-addi  x7,  x0,  -2		# Place -2 in x7
-addi  x8,  x0,  -3		# Place -3 in x8
-addi  x9,  x0,  -4		# Place -4 in x9
-addi  x10, x0,  -5        # Place -5 in x10
-
-# sub
-sub   x11, x1,  x2		# Place 1 - 2  in x11
-sub   x12, x1,  x3		# Place 1 - 3  in x12
-sub   x13, x1,  x4        # Place 1 - 4  in x13
-sub   x14, x1,  x5        # Place 1 - 5  in x14
-sub   x15, x2,  x1        # Place 2 - 1  in x15
-
-# Store
-addi t1, x0, 1024
-add t1, t1, t1
-add t1, t1, t1
-
-addi t1, t1, 1
-
-add t1, t1, t1
-add t1, t1, t1
-add t1, t1, t1
-add t1, t1, t1
-add t1, t1, t1
-add t1, t1, t1
-add t1, t1, t1
-add t1, t1, t1
-add t1, t1, t1
-add t1, t1, t1
-add t1, t1, t1
-add t1, t1, t1
-add t1, t1, t1
-add t1, t1, t1
-add t1, t1, t1
-add t1, t1, t1
-
-sw x1, 0(t1)
-
-lw x5, 0(t1)
-
-# TODO: Implement Load
-
+addi  x6,  x0,  6		# Place 6  in x6
+addi  x7,  x0,  7		# Place 7  in x7
+addi  x8,  x0,  8		# Place 8  in x8
+addi  x9,  x0,  9		# Place 9  in x9
+addi  x10, x0,  10		# Place 10 in x10
 wfi
